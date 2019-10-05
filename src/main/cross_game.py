7f9106d9bf41762f3509e52d051d5fd591a85075
thomas <thomaspesneau@gmail.com>
2019-10-05 18:40:41 +0200

    Implemented CrossGame interface with method to put token in grid

---
+++ src/main/cross_game.py
@@ -0,0 +1,23 @@
+
+class CrossGame:
+    
+    def __init__(self):
+        self._grid = [[0 for _ in range(6)] for _ in range(7)]
+
+    def put_token(self, col_index, agent_id):
+        if agent_id == 0:
+            raise ZeroAgentIdError()
+        if self._grid[col_index][5] != 0:
+            raise ColumnIsFullError(col_index)
+        for i, slot in enumerate(self._grid[col_index]):
+            if slot == 0:
+                self._grid[col_index][i] = agent_id
+                break
+
+class ZeroAgentIdError(Exception):
+    def __init__(self):
+        self.message = "Agent id should not be equal zero which is the convention for empty cell."
+
+class ColumnIsFullError(Exception):
+    def __init__(self, col_index):
+        self.message = "You cannot play in column {} because it is full.".format(col_index)
