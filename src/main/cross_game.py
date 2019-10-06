65c73d38254250a17cdc931cf74dd761fba51208
thomas <thomaspesneau@gmail.com>
2019-10-06 12:04:27 +0200

    Raise OutOfGrid Exception when a player attempts to play out of the grid

--- src/main/cross_game.py
+++ src/main/cross_game.py
@@ -1,13 +1,20 @@
 
 class CrossGame:
-    
+
     def __init__(self):
-        self._grid = [[0 for _ in range(6)] for _ in range(7)]
+        self._NB_COLUMNS = 7
+        self._NB_ROWS = 6
+        self._init_grid()
+
+    def _init_grid(self):
+        self._grid = [[0 for _ in range(self._NB_ROWS)] for _ in range(self._NB_COLUMNS)]
 
     def put_token(self, col_index, agent_id):
         if agent_id == 0:
             raise ZeroAgentIdError()
-        if self._grid[col_index][5] != 0:
+        if col_index >= self._NB_COLUMNS:
+            raise OutOfGridError(agent_id, col_index, self._NB_COLUMNS)
+        if self._grid[col_index][self._NB_ROWS - 1] != 0:
             raise ColumnIsFullError(col_index)
         for i, slot in enumerate(self._grid[col_index]):
             if slot == 0:
@@ -21,3 +28,9 @@ class ZeroAgentIdError(Exception):
 class ColumnIsFullError(Exception):
     def __init__(self, col_index):
         self.message = "You cannot play in column {} because it is full.".format(col_index)
+
+class OutOfGridError(Exception):
+    def __init__(self, agent_id, col_index, nb_cols):
+        self.message = "Player {} has selected selected column {} but there are only {} columns".format(agent_id,
+                                                                                                        col_index,
+                                                                                                        nb_cols)
--- src/test/test_cross_game.py
+++ src/test/test_cross_game.py
@@ -4,9 +4,9 @@ from ..main import cross_game
 
 def test_new_instance_should_have_an_empty_grid_attribute():
     ## Given
-    expected_grid = [[0 for _ in range(6)] for _ in range(7)]
-    ## When
     actual_game = cross_game.CrossGame()
+    expected_grid = [[0 for _ in range(actual_game._NB_ROWS)] for _ in range(actual_game._NB_COLUMNS)]
+    ## When
     actual_grid = actual_game._grid
     ## Then
     assert(actual_grid == expected_grid)
@@ -14,7 +14,7 @@ def test_new_instance_should_have_an_empty_grid_attribute():
 def test_put_token_should_write_a_token_in_the_available_index():
     ## Given
     actual_game = cross_game.CrossGame()
-    expected_grid = [[0 for _ in range(6)] for _ in range(7)]
+    expected_grid = [[0 for _ in range(actual_game._NB_ROWS)] for _ in range(actual_game._NB_COLUMNS)]
     expected_grid[0][0] = 1
     ## When
     actual_game.put_token(0, 1)
@@ -25,7 +25,7 @@ def test_put_token_should_write_a_token_in_the_available_index():
 def test_put_token_should_write_another_token_above_a_previous_one():
     ## Given
     actual_game = cross_game.CrossGame()
-    expected_grid = [[0 for _ in range(6)] for _ in range(7)]
+    expected_grid = [[0 for _ in range(actual_game._NB_ROWS)] for _ in range(actual_game._NB_COLUMNS)]
     expected_grid[0][0] = 1
     expected_grid[0][1] = 2
     ## When
@@ -38,7 +38,6 @@ def test_put_token_should_write_another_token_above_a_previous_one():
 def test_put_token_should_throw_exception_when_agent_id_equals_zero():
     ## Given
     actual_game = cross_game.CrossGame()
-    expected_grid = [[0 for _ in range(6)] for _ in range(7)]
     ## When
     with pytest.raises(cross_game.ZeroAgentIdError):
         actual_game.put_token(0, 0)
@@ -47,8 +46,16 @@ def test_put_token_should_throw_exception_when_column_is_full():
     ## Given
     actual_game = cross_game.CrossGame()
     ## When
-    for _ in range(6):
+    for _ in range(actual_game._NB_ROWS):
         actual_game.put_token(0, 1)
     ## Then
     with pytest.raises(cross_game.ColumnIsFullError):
         actual_game.put_token(0, 1)
+
+def test_put_token_should_throw_exception_when_player_tries_to_play_outside_the_grid():
+    ## Given
+    actual_game = cross_game.CrossGame()
+    ## When
+    ## Then
+    with pytest.raises(cross_game.OutOfGridError):
+        actual_game.put_token(10, 1)
