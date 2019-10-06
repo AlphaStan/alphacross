a4538b13ec56c581be85608976cc2776178e62b4
thomas <thomaspesneau@gmail.com>
2019-10-06 12:43:20 +0200

    Define cross game custom exceptions in separate file

--- src/main/cross_game.py
+++ src/main/cross_game.py
@@ -1,3 +1,5 @@
+from .errors import ZeroAgentIdError, ColumnIsFullError, OutOfGridError, AlreadyPlayedError
+
 
 class CrossGame:
 
@@ -33,25 +35,3 @@ class CrossGame:
                 self._grid[col_index][i] = agent_id
                 self._last_player_agent_id = agent_id
                 break
-
-
-class ZeroAgentIdError(Exception):
-    def __init__(self):
-        self.message = "Agent id should not be equal zero which is the convention for empty cell."
-
-
-class ColumnIsFullError(Exception):
-    def __init__(self, col_index):
-        self.message = "You cannot play in column {} because it is full.".format(col_index)
-
-
-class OutOfGridError(Exception):
-    def __init__(self, agent_id, col_index, nb_cols):
-        self.message = "Player {} has selected selected column {} but there are only {} columns".format(agent_id,
-                                                                                                        col_index,
-                                                                                                        nb_cols)
-
-
-class AlreadyPlayedError(Exception):
-    def __init__(self, agent_id):
-        self.message = "Player {} has already played, cannot play twice in a row".format(agent_id)
---
+++ src/main/errors.py
@@ -0,0 +1,20 @@
+class ZeroAgentIdError(Exception):
+    def __init__(self):
+        self.message = "Agent id should not be equal zero which is the convention for empty cell."
+
+
+class ColumnIsFullError(Exception):
+    def __init__(self, col_index):
+        self.message = "You cannot play in column {} because it is full.".format(col_index)
+
+
+class OutOfGridError(Exception):
+    def __init__(self, agent_id, col_index, nb_cols):
+        self.message = "Player {} has selected selected column {} but there are only {} columns".format(agent_id,
+                                                                                                        col_index,
+                                                                                                        nb_cols)
+
+
+class AlreadyPlayedError(Exception):
+    def __init__(self, agent_id):
+        self.message = "Player {} has already played, cannot play twice in a row".format(agent_id)
