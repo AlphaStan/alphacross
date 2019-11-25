bc7fecd86168bf1d4c09b60674d5e3b83391467a
thomas <thomaspesneau@gmail.com>
2019-11-25 23:31:53 +0100

    Remove unused abstract methods

--- src/main/environment.py
+++ src/main/environment.py
@@ -6,12 +6,6 @@ class _Environment(ABC):
     def __init__(self):
         super().__init__()
 
-    def evaluate_state_action(self, state, action):
-        raise NotImplementedError
-
-    def evaluate_state(self, state):
-        raise NotImplementedError
-
     @abstractmethod
     def apply_action(self, action):
         raise NotImplementedError
