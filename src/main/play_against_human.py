d93bebe9365f27a4b8b4e76bbaf842786bd01f64
desrousseaux <quentin.desrousseaux@gmail.com>
2019-12-20 20:24:48 +0100

    modify main files

--- src/main/play_against_human.py
+++ src/main/play_against_human.py
@@ -1,9 +1,6 @@
 from src.main.cross_game import CrossGame
 
 
-def play_against_human():
+if __name__ == "__main__":
     game = CrossGame()
     game.play_game_against_human()
-
-if __name__ == "__main__":
-    play_against_human()
---
+++ src/main/play_against_ia.py
@@ -0,0 +1,6 @@
+from src.main.cross_game import CrossGame
+
+
+if __name__ == "__main__":
+    game = CrossGame()
+    game.play_game_against_ai()
