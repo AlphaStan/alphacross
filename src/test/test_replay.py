2cbeb9da23732af35917e4dde0e2ffd4fc4ef99f
thomas <thomaspesneau@gmail.com>
2020-04-21 16:55:30 +0200

    Rebase on master

--- src/test/test_replay.py
+++ src/test/test_replay.py
@@ -1,14 +1,11 @@
-import pytest
-import sys
-sys.path.append('../models')
-import replay
+from src.main.models.replay import Replay
 
 
-def test_toggle_state_should_toggle_player_ids_in_grid():
+def test_classmethod_toggle_state_should_toggle_player_ids_in_grid():
     # GIVEN
     state = [[1, 1, 1], [2, 2, 1], [0, 1, 2]]
     expected_state = [[2, 2, 2], [1, 1, 2], [0, 2, 1]]
     # WHEN
-    actual_state = replay.Replay.toggle_state(state)
+    actual_state = Replay.toggle_state(state)
     # THEN
     assert actual_state == expected_state
