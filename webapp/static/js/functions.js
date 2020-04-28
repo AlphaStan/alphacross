2a2f2707f9aa988d344651986c837067b578d213
thomas <thomaspesneau@gmail.com>
2020-04-28 15:38:24 +0200

    Move grid handler callback to functions.js

--- webapp/static/js/functions.js
+++ webapp/static/js/functions.js
@@ -1,4 +1,4 @@
-function reset_board(result, status, xhr){
+function resetBoard(result, status, xhr){
     console.log("Reset board");
     // Reset buttons class
     var buttons = board.getElementsByTagName('button');
@@ -17,6 +17,41 @@ function reset_board(result, status, xhr){
 }
 
 
-function default_error(xhr, status, error) {
+function defaultError(xhr, status, error) {
     console.log(xhr.status + ": " + xhr.responseText);
+}
+
+
+function handleGridUpdate(result, status, xhr){
+    var parsed_result = $.parseJSON(result);
+    var buttonClass = (parsed_result["player_id"] == 1) ? "red" : "yellow";
+    var row = document.querySelector('tr:nth-child(' + (1 + parsed_result["row_id"]) + ')');
+    var cell = row.querySelector('td:nth-child(' + (1 + parsed_result["col_id"]) + ')');
+    if (parsed_result["column_is_full"]) {
+        document.getElementById('msg').innerHTML = 'Sorry, column ' + (1 + parsed_result["col_id"]) + ' is full';
+    }
+    else{
+        cell.firstElementChild.classList.add(buttonClass);
+        document.getElementById('msg').innerHTML = '';
+    }
+    if (parsed_result["has_won"]) {
+        if (activeAI){
+            document.getElementById('msg').innerHTML = 'You have won!';
+        }
+        else{
+            document.getElementById('msg').innerHTML = 'Player ' + parsed_result["player_id"] + ' has won!';
+        }
+        gameIsFinished = true;
+    }
+    if (activeAI & !parsed_result["column_is_full"] & !parsed_result["has_won"]) {
+        var agentButtonClass = (parsed_result["agent_id"] == 2) ? "yellow" : "red";
+        var row = document.querySelector('tr:nth-child(' + (1 + parsed_result["agent_row_id"]) + ')');
+        var cell = row.querySelector('td:nth-child(' + (1 + parsed_result["agent_col_id"]) + ')');
+        cell.firstElementChild.classList.add(agentButtonClass);
+        document.getElementById('msg').innerHTML = '';
+        if (parsed_result["agent_has_won"]) {
+            document.getElementById('msg').innerHTML = 'You have lost!';
+            gameIsFinished = true;
+        }
+    }
 }
--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -21,40 +21,8 @@ function sendActionToFlask(column_id){
     $.ajax({
         url: urlToPost,
         type: 'GET',
-        success: function(result, status, xhr){
-            var parsed_result = $.parseJSON(result);
-            var buttonClass = (parsed_result["player_id"] == 1) ? "red" : "yellow";
-            var row = document.querySelector('tr:nth-child(' + (1 + parsed_result["row_id"]) + ')');
-            var cell = row.querySelector('td:nth-child(' + (1 + parsed_result["col_id"]) + ')');
-            if (parsed_result["column_is_full"]) {
-                document.getElementById('msg').innerHTML = 'Sorry, column ' + (1 + parsed_result["col_id"]) + ' is full';
-            }
-            else{
-                cell.firstElementChild.classList.add(buttonClass);
-                document.getElementById('msg').innerHTML = '';
-            }
-            if (parsed_result["has_won"]) {
-                if (activeAI){
-                    document.getElementById('msg').innerHTML = 'You have won!';
-                }
-                else{
-                    document.getElementById('msg').innerHTML = 'Player ' + parsed_result["player_id"] + ' has won!';
-                }
-                gameIsFinished = true;
-            }
-            if (activeAI & !parsed_result["column_is_full"] & !parsed_result["has_won"]) {
-                var agentButtonClass = (parsed_result["agent_id"] == 2) ? "yellow" : "red";
-                var row = document.querySelector('tr:nth-child(' + (1 + parsed_result["agent_row_id"]) + ')');
-                var cell = row.querySelector('td:nth-child(' + (1 + parsed_result["agent_col_id"]) + ')');
-                cell.firstElementChild.classList.add(agentButtonClass);
-                document.getElementById('msg').innerHTML = '';
-                if (parsed_result["agent_has_won"]) {
-                    document.getElementById('msg').innerHTML = 'You have lost!';
-                    gameIsFinished = true;
-                }
-            }
-        },
-        error: default_error
+        success: handleGridUpdate,
+        error: defaultError
     });
 }
 
@@ -77,8 +45,8 @@ function sendResetSignal(s){
     $.ajax({
         url: urlToReset,
         type: 'GET',
-        success: reset_board,
-        error: default_error
+        success: resetBoard,
+        error: defaultError
     });
 }
 
@@ -104,7 +72,7 @@ function resetVariables(){
     $.ajax({
         url: urlToReset,
         type: 'GET',
-        success: reset_board,
-        error: default_error
+        success: resetBoard,
+        error: defaultError
     });
 }
