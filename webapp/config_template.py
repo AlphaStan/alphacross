e61fc75b56ebbd84f1b8ac80720860f6106e43bd
thomas <thomaspesneau@gmail.com>
2020-05-03 13:37:53 +0200

    Add template file for flask config

---
+++ webapp/config_template.py
@@ -0,0 +1,5 @@
+DEBUG = 'True/False'
+TEMPLATE_FOLDER = "your template folder (.html files)"
+STATIC_FOLDER = "your static folder (.css, .js)"
+PANE_TITLE = "AlphaCross"
+MODEL_PATH = "your path to saved model (.h5)"
