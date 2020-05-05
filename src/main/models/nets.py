2a60c285a9e0d777f12a791931582c992846841d
thomas <thomaspesneau@gmail.com>
2020-05-06 01:17:04 +0200

    Add input shape parameter to convnet

--- src/main/models/nets.py
+++ src/main/models/nets.py
@@ -22,13 +22,14 @@ class CFDense:
 
 class CFConv1:
 
-    def __init__(self, n_actions, trainable=True):
+    def __init__(self, n_actions, input_shape, trainable=True):
         self.n_actions = n_actions
         self.trainable = trainable
+        self.input_shape = input_shape
 
     def get_model(self):
         model = tf.keras.Sequential()
-        model.add(Conv2D(3, kernel_size=3, trainable=self.trainable))
+        model.add(Conv2D(3, kernel_size=3, trainable=self.trainable, input_shape=self.input_shape))
         model.add(Dropout(0.5, trainable=self.trainable))
         model.add(Flatten())
         model.add(Dense(self.n_actions, activation='softmax', trainable=self.trainable))
