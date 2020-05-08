b1d400c6448937b6cffbc73947d4a0926bd53076
thomas <thomaspesneau@gmail.com>
2020-05-08 17:28:38 +0200

    Fix typo

--- src/main/utils.py
+++ src/main/utils.py
@@ -1,17 +1,17 @@
 import os
 from tensorflow.keras.models import load_model
 
-from .constants import PATH_TO_MODELS
 from .models.dqn_agent import dqn_mask_loss
 
 
-def choose_model(choose_model=False):
-    path_to_models = PATH_TO_MODELS
-    sorted_models = sorted([f for f in os.listdir(path_to_models) if os.isfile(os.join(path_to_models, f))], reverse=True)
+def choose_model(path_to_models, choose=False):
+    sorted_models = sorted([f for f in os.listdir(path_to_models)
+                            if os.path.isfile(os.path.join(path_to_models, f)) and os.path.splitext(f)[1] == '.h5'],
+                           reverse=True)
     if not sorted_models:
         return None
 
-    if choose_model:
+    if choose:
         n = len(sorted_models)
         counter = 0
         input_index = -1
@@ -19,7 +19,7 @@ def choose_model(choose_model=False):
         for index, model in enumerate(sorted_models):
             print("{} : {}".format(index, model))
         while counter < 5 and (input_index < 0 or input_index > n-1):
-            input_index = input("Give the id of the model you want :")
+            input_index = int(input("Give the id of the model you want :"))
             counter += 1
 
         if counter == 5:
@@ -27,8 +27,8 @@ def choose_model(choose_model=False):
             return
 
         model = sorted_models[input_index]
-        return load_model(os.join(path_to_models, model), custom_objects = {'dqn_mask_loss': dqn_mask_loss})
+        return load_model(os.path.join(path_to_models, model), custom_objects={'dqn_mask_loss': dqn_mask_loss})
 
     else:
         model = sorted_models[0]
-        return load_model(os.join(path_to_models, model), custom_objects = {'dqn_mask_loss': dqn_mask_loss})
+        return load_model(os.path.join(path_to_models, model), custom_objects={'dqn_mask_loss': dqn_mask_loss})
---
+++ src/play_against_agent.py
@@ -0,0 +1,19 @@
+import click
+
+from main.environment.cross_game import CrossGame
+from main.models.dqn_agent import DQNAgent
+from main.utils import choose_model
+
+
+@click.command()
+@click.option('--path-to-models', type=click.Path(exists=True), default='../models', help='Path to trained models')
+def play_against_agent(path_to_models):
+    game = CrossGame()
+    agent = DQNAgent(game)
+    model = choose_model(path_to_models, True)
+    agent.model = model
+    game.play_game_against_agent(agent)
+
+
+if __name__ == "__main__":
+    play_against_agent()
