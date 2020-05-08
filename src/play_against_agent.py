fd4a4527f22fc4afea0c8286bfec0efbf040a870
thomas <thomaspesneau@gmail.com>
2020-05-08 17:39:57 +0200

    Add choose model option in play against agent

--- src/play_against_agent.py
+++ src/play_against_agent.py
@@ -7,10 +7,11 @@ from main.utils import choose_model
 
 @click.command()
 @click.option('--path-to-models', type=click.Path(exists=True), default='../models', help='Path to trained models')
-def play_against_agent(path_to_models):
+@click.option('--choose', type=bool, default=False, help='If True, choose the model in terminal')
+def play_against_agent(path_to_models, choose):
     game = CrossGame()
     agent = DQNAgent(game)
-    model = choose_model(path_to_models, True)
+    model = choose_model(path_to_models, choose=choose)
     agent.model = model
     game.play_game_against_agent(agent)
 
