04ad7c47930cb020803b07c84d7a23bf97bd3719
thomas <thomaspesneau@gmail.com>
2020-05-15 11:00:05 +0200

    Refactor _make_one_move and percentages in RandomAgentEvaluator

--- src/main/evaluation/win_percentage.py
+++ src/main/evaluation/win_percentage.py
@@ -9,29 +9,36 @@ class RandomAgentEvaluator:
         self.agent = agent
         self.environment = environment
         self.num_episodes = num_episodes
+        self.random_agent_id = 1
+        self.agent_id = 2
+        self._percentages = self._init_percentages()
+
+    @staticmethod
+    def _init_percentages():
+        return {'agent_winning_percentage': 0, 'random_agent_winning_percentage': 0, 'draw_percentage': 0}
 
     def _make_one_move(self, action, player_id):
         has_played = False
-        has_won = False
-        is_blocked = False
+        episode_is_finished = False
         try:
             _, new_state = self.environment.apply_action(action)
             has_played = True
             if self.environment._is_winning_move(new_state, action, player_id):
-                has_won = True
+                if player_id == 1:
+                    self._percentages['random_agent_winning_percentage'] += 1. / self.num_episodes
+                else:
+                    self._percentages['agent_winning_percentage'] += 1. / self.num_episodes
+                episode_is_finished = True
             elif self.environment.is_blocked():
-                is_blocked = True
+                self._percentages['draw_percentage'] += 1. / self.num_episodes
+                episode_is_finished = True
         except ColumnIsFullError:
             pass
-        return has_played, has_won, is_blocked
+        return has_played, episode_is_finished
 
     def evaluate(self):
         n_actions = self.environment.get_action_space_size()
-        random_agent_id = 1
-        agent_id = 2
-        percentages = {'agent_winning_percentage': 0,
-                       'random_agent_winning_percentage': 0,
-                       'draw_percentage': 0}
+        self._percentages = self._init_percentages()
         for episode_index in range(self.num_episodes):
             self.environment.reset()
             episode_is_finished = False
@@ -40,26 +47,10 @@ class RandomAgentEvaluator:
                 agent_has_played = False
                 while not random_agent_has_played:
                     random_agent_action = np.random.randint(n_actions)
-                    random_agent_has_played, random_agent_has_won, environment_is_blocked = self._make_one_move(
-                        random_agent_action,
-                        random_agent_id)
-                    if random_agent_has_won:
-                        percentages['random_agent_winning_percentage'] += 1
-                        episode_is_finished = True
-                    elif environment_is_blocked:
-                        percentages['draw_percentage'] += 1
-                        episode_is_finished = True
+                    random_agent_has_played, episode_is_finished = self._make_one_move(random_agent_action,
+                                                                                       self.random_agent_id)
                 while not agent_has_played and not episode_is_finished:
                     agent_action = self.agent.select_action(self.environment)
-                    agent_has_played, agent_has_won, environment_is_blocked = self._make_one_move(
-                        agent_action,
-                        agent_id)
-                    if agent_has_won:
-                        percentages['agent_winning_percentage'] += 1
-                        episode_is_finished = True
-                    elif environment_is_blocked:
-                        percentages['draw_percentage'] += 1
-                        episode_is_finished = True
-        for key in percentages:
-            percentages[key] /= self.num_episodes
-        return percentages
+                    agent_has_played, episode_is_finished = self._make_one_move(agent_action,
+                                                                                self.agent_id)
+        return self._percentages
